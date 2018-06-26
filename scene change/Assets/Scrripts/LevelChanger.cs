ec804d0af1b7d6da1859a76ebd525f7edccdcc5d
Jesus-1996 <38133794+koussaykhelil@users.noreply.github.com>
2018-06-26 15:07:34 +0100

    fade

---
+++ scene change/Assets/Scrripts/LevelChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+    public int scenetoload;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            FadeToLevel(1);
+		
+	}
+
+
+
+    public void FadeToLevel (int index)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(scenetoload);
+    }
+}
